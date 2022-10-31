2711abd2836e7d591eb7a45b6912efd0b65484cb
RomanMamondez <112417045+Romannn10@users.noreply.github.com>
2022-10-31 11:31:10 -0300

    Add files via upload

---
+++ pryMamondezIEFI/frmCantidadClientes.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace pryMamondezIEFI
+{
+    partial class frmCantidadClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mrcConsultaDatos = new System.Windows.Forms.GroupBox();
+            this.lblMontoPromedio = new System.Windows.Forms.Label();
+            this.lblPromedio = new System.Windows.Forms.Label();
+            this.lblMonto = new System.Windows.Forms.Label();
+            this.lblMontoTotal = new System.Windows.Forms.Label();
+            this.cmdListar = new System.Windows.Forms.Button();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblCantidadClientes = new System.Windows.Forms.Label();
+            this.dgvDeudores = new System.Windows.Forms.DataGridView();
+            this.statusConexion = new System.Windows.Forms.StatusStrip();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.mrcConsultaDatos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDeudores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mrcConsultaDatos
+            // 
+            this.mrcConsultaDatos.Controls.Add(this.lblMontoPromedio);
+            this.mrcConsultaDatos.Controls.Add(this.lblPromedio);
+            this.mrcConsultaDatos.Controls.Add(this.lblMonto);
+            this.mrcConsultaDatos.Controls.Add(this.lblMontoTotal);
+            this.mrcConsultaDatos.Controls.Add(this.cmdListar);
+            this.mrcConsultaDatos.Controls.Add(this.lblCantidad);
+            this.mrcConsultaDatos.Controls.Add(this.lblCantidadClientes);
+            this.mrcConsultaDatos.Controls.Add(this.dgvDeudores);
+            this.mrcConsultaDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mrcConsultaDatos.ForeColor = System.Drawing.SystemColors.Control;
+            this.mrcConsultaDatos.Location = new System.Drawing.Point(23, 12);
+            this.mrcConsultaDatos.Name = "mrcConsultaDatos";
+            this.mrcConsultaDatos.Size = new System.Drawing.Size(756, 471);
+            this.mrcConsultaDatos.TabIndex = 0;
+            this.mrcConsultaDatos.TabStop = false;
+            this.mrcConsultaDatos.Text = "Consulta de datos";
+            this.mrcConsultaDatos.Enter += new System.EventHandler(this.mrcConsultaDatos_Enter);
+            // 
+            // lblMontoPromedio
+            // 
+            this.lblMontoPromedio.BackColor = System.Drawing.Color.LightGray;
+            this.lblMontoPromedio.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMontoPromedio.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblMontoPromedio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMontoPromedio.Location = new System.Drawing.Point(611, 402);
+            this.lblMontoPromedio.Name = "lblMontoPromedio";
+            this.lblMontoPromedio.Size = new System.Drawing.Size(120, 24);
+            this.lblMontoPromedio.TabIndex = 14;
+            // 
+            // lblPromedio
+            // 
+            this.lblPromedio.AutoSize = true;
+            this.lblPromedio.Location = new System.Drawing.Point(455, 402);
+            this.lblPromedio.Name = "lblPromedio";
+            this.lblPromedio.Size = new System.Drawing.Size(111, 17);
+            this.lblPromedio.TabIndex = 13;
+            this.lblPromedio.Text = "Monto Promedio";
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.BackColor = System.Drawing.Color.LightGray;
+            this.lblMonto.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMonto.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblMonto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMonto.Location = new System.Drawing.Point(611, 368);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(120, 24);
+            this.lblMonto.TabIndex = 12;
+            // 
+            // lblMontoTotal
+            // 
+            this.lblMontoTotal.AutoSize = true;
+            this.lblMontoTotal.Location = new System.Drawing.Point(455, 368);
+            this.lblMontoTotal.Name = "lblMontoTotal";
+            this.lblMontoTotal.Size = new System.Drawing.Size(158, 17);
+            this.lblMontoTotal.TabIndex = 11;
+            this.lblMontoTotal.Text = "Total Dinero Facturado:";
+            // 
+            // cmdListar
+            // 
+            this.cmdListar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.cmdListar.Location = new System.Drawing.Point(649, 437);
+            this.cmdListar.Name = "cmdListar";
+            this.cmdListar.Size = new System.Drawing.Size(82, 28);
+            this.cmdListar.TabIndex = 10;
+            this.cmdListar.Text = "Listar";
+            this.cmdListar.UseVisualStyleBackColor = true;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.BackColor = System.Drawing.Color.LightGray;
+            this.lblCantidad.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCantidad.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(611, 334);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(120, 24);
+            this.lblCantidad.TabIndex = 8;
+            this.lblCantidad.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblCantidadClientes
+            // 
+            this.lblCantidadClientes.AutoSize = true;
+            this.lblCantidadClientes.Location = new System.Drawing.Point(455, 334);
+            this.lblCantidadClientes.Name = "lblCantidadClientes";
+            this.lblCantidadClientes.Size = new System.Drawing.Size(140, 17);
+            this.lblCantidadClientes.TabIndex = 4;
+            this.lblCantidadClientes.Text = "Cantidad de clientes:";
+            // 
+            // dgvDeudores
+            // 
+            this.dgvDeudores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDeudores.Location = new System.Drawing.Point(22, 33);
+            this.dgvDeudores.Name = "dgvDeudores";
+            this.dgvDeudores.Size = new System.Drawing.Size(709, 285);
+            this.dgvDeudores.TabIndex = 1;
+            // 
+            // statusConexion
+            // 
+            this.statusConexion.Location = new System.Drawing.Point(0, 491);
+            this.statusConexion.Name = "statusConexion";
+            this.statusConexion.Size = new System.Drawing.Size(800, 22);
+            this.statusConexion.TabIndex = 3;
+            this.statusConexion.Text = "statusStrip1";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.BackColor = System.Drawing.Color.White;
+            this.lblEstado.Location = new System.Drawing.Point(12, 496);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(10, 13);
+            this.lblEstado.TabIndex = 4;
+            this.lblEstado.Text = "-";
+            // 
+            // frmCantidadClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(800, 513);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.statusConexion);
+            this.Controls.Add(this.mrcConsultaDatos);
+            this.Name = "frmCantidadClientes";
+            this.Text = "Lista Cantidad de Clientes";
+            this.Load += new System.EventHandler(this.frmCantidadClientes_Load);
+            this.mrcConsultaDatos.ResumeLayout(false);
+            this.mrcConsultaDatos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDeudores)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox mrcConsultaDatos;
+        private System.Windows.Forms.DataGridView dgvDeudores;
+        private System.Windows.Forms.Label lblCantidadClientes;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Button cmdListar;
+        private System.Windows.Forms.Label lblMontoPromedio;
+        private System.Windows.Forms.Label lblPromedio;
+        private System.Windows.Forms.Label lblMonto;
+        private System.Windows.Forms.Label lblMontoTotal;
+        private System.Windows.Forms.StatusStrip statusConexion;
+        private System.Windows.Forms.Label lblEstado;
+    }
+}
---
+++ pryMamondezIEFI/frmSistema.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace pryMamondezIEFI
+{
+    partial class frmSistema
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblContacto = new System.Windows.Forms.Label();
+            this.lblNombree = new System.Windows.Forms.Label();
+            this.lblApellidoo = new System.Windows.Forms.Label();
+            this.lblContactoo = new System.Windows.Forms.Label();
+            this.cmdVolver = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(12, 23);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(78, 22);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.Location = new System.Drawing.Point(12, 60);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(79, 22);
+            this.lblApellido.TabIndex = 1;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // lblContacto
+            // 
+            this.lblContacto.AutoSize = true;
+            this.lblContacto.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContacto.Location = new System.Drawing.Point(12, 97);
+            this.lblContacto.Name = "lblContacto";
+            this.lblContacto.Size = new System.Drawing.Size(82, 22);
+            this.lblContacto.TabIndex = 2;
+            this.lblContacto.Text = "Contacto";
+            // 
+            // lblNombree
+            // 
+            this.lblNombree.AutoSize = true;
+            this.lblNombree.BackColor = System.Drawing.Color.Gold;
+            this.lblNombree.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNombree.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombree.ForeColor = System.Drawing.Color.Blue;
+            this.lblNombree.Location = new System.Drawing.Point(117, 23);
+            this.lblNombree.Name = "lblNombree";
+            this.lblNombree.Size = new System.Drawing.Size(69, 24);
+            this.lblNombree.TabIndex = 3;
+            this.lblNombree.Text = "Roman";
+            // 
+            // lblApellidoo
+            // 
+            this.lblApellidoo.AutoSize = true;
+            this.lblApellidoo.BackColor = System.Drawing.Color.Gold;
+            this.lblApellidoo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblApellidoo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellidoo.ForeColor = System.Drawing.Color.Blue;
+            this.lblApellidoo.Location = new System.Drawing.Point(117, 62);
+            this.lblApellidoo.Name = "lblApellidoo";
+            this.lblApellidoo.Size = new System.Drawing.Size(98, 24);
+            this.lblApellidoo.TabIndex = 4;
+            this.lblApellidoo.Text = "Mamondez";
+            // 
+            // lblContactoo
+            // 
+            this.lblContactoo.AutoSize = true;
+            this.lblContactoo.BackColor = System.Drawing.Color.Gold;
+            this.lblContactoo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblContactoo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContactoo.ForeColor = System.Drawing.Color.Blue;
+            this.lblContactoo.Location = new System.Drawing.Point(117, 101);
+            this.lblContactoo.Name = "lblContactoo";
+            this.lblContactoo.Size = new System.Drawing.Size(283, 24);
+            this.lblContactoo.TabIndex = 5;
+            this.lblContactoo.Text = "Romanmamondez010@gmail.com";
+            // 
+            // cmdVolver
+            // 
+            this.cmdVolver.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdVolver.Location = new System.Drawing.Point(330, 140);
+            this.cmdVolver.Name = "cmdVolver";
+            this.cmdVolver.Size = new System.Drawing.Size(70, 30);
+            this.cmdVolver.TabIndex = 6;
+            this.cmdVolver.Text = "Volver";
+            this.cmdVolver.UseVisualStyleBackColor = true;
+            this.cmdVolver.Click += new System.EventHandler(this.cmdVolver_Click);
+            // 
+            // frmSistema
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(420, 184);
+            this.Controls.Add(this.cmdVolver);
+            this.Controls.Add(this.lblContactoo);
+            this.Controls.Add(this.lblApellidoo);
+            this.Controls.Add(this.lblNombree);
+            this.Controls.Add(this.lblContacto);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "frmSistema";
+            this.Text = "Sistema";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblContacto;
+        private System.Windows.Forms.Label lblNombree;
+        private System.Windows.Forms.Label lblApellidoo;
+        private System.Windows.Forms.Label lblContactoo;
+        private System.Windows.Forms.Button cmdVolver;
+    }
+}
