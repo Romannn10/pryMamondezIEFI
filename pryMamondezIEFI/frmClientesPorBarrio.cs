0193b767f780bcc2ee6d4ffff342aa4588e6a483
RomanMamondez <112417045+Romannn10@users.noreply.github.com>
2022-11-03 07:53:00 -0300

    Add files via upload

---
+++ pryMamondezIEFI/frmClientesPorBarrio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace pryMamondezIEFI
+{
+    public partial class frmClientesPorBarrio : Form
+    {
+        public frmClientesPorBarrio()
+        {
+            InitializeComponent();
+        }
+
+        private void frmClientesPorBarrio_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
