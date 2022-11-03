62afe2d9e1deb0bed5c614392c7c7082e1e85f9a
RomanMamondez <112417045+Romannn10@users.noreply.github.com>
2022-11-03 10:18:39 -0300

    Add files via upload

---
+++ pryMamondezIEFI/frmCantidadClientes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using pryMamondezIEFI.Resources;
+
+namespace pryMamondezIEFI
+{
+    public partial class frmCantidadClientes : Form
+    {
+        public frmCantidadClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTotalDeuda_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mrcConsultaDatos_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmCantidadClientes_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdListar_Click(object sender, EventArgs e)
+        {
+            ClsClientes objCantidadClientes = new ClsClientes();
+            objCantidadClientes.Listar(dgvDeudores);
+        }
+    }
+}
---
+++ pryMamondezIEFI/frmPrincipal.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace pryMamondezIEFI
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.msMain = new System.Windows.Forms.MenuStrip();
+            this.sistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarNuevosClientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buscarClientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listadoDeClientesDeudoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listadoDeClientesDeUnaCiudadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pbImagen = new System.Windows.Forms.PictureBox();
+            this.statusConexion = new System.Windows.Forms.StatusStrip();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.msMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // msMain
+            // 
+            this.msMain.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.msMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sistemaToolStripMenuItem,
+            this.clientesToolStripMenuItem});
+            this.msMain.Location = new System.Drawing.Point(0, 0);
+            this.msMain.Name = "msMain";
+            this.msMain.Size = new System.Drawing.Size(420, 27);
+            this.msMain.TabIndex = 0;
+            this.msMain.Text = "menuStrip1";
+            this.msMain.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.msMain_ItemClicked);
+            // 
+            // sistemaToolStripMenuItem
+            // 
+            this.sistemaToolStripMenuItem.Name = "sistemaToolStripMenuItem";
+            this.sistemaToolStripMenuItem.Size = new System.Drawing.Size(68, 23);
+            this.sistemaToolStripMenuItem.Text = "Sistema";
+            this.sistemaToolStripMenuItem.Click += new System.EventHandler(this.sistemaToolStripMenuItem_Click);
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.agregarNuevosClientesToolStripMenuItem,
+            this.buscarClientesToolStripMenuItem,
+            this.listadoDeClientesDeudoresToolStripMenuItem,
+            this.listadoDeClientesDeUnaCiudadToolStripMenuItem});
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(69, 23);
+            this.clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // agregarNuevosClientesToolStripMenuItem
+            // 
+            this.agregarNuevosClientesToolStripMenuItem.Name = "agregarNuevosClientesToolStripMenuItem";
+            this.agregarNuevosClientesToolStripMenuItem.Size = new System.Drawing.Size(280, 24);
+            this.agregarNuevosClientesToolStripMenuItem.Text = "Agregar nuevos clientes...";
+            this.agregarNuevosClientesToolStripMenuItem.Click += new System.EventHandler(this.agregarNuevosClientesToolStripMenuItem_Click);
+            // 
+            // buscarClientesToolStripMenuItem
+            // 
+            this.buscarClientesToolStripMenuItem.Name = "buscarClientesToolStripMenuItem";
+            this.buscarClientesToolStripMenuItem.Size = new System.Drawing.Size(280, 24);
+            this.buscarClientesToolStripMenuItem.Text = "Buscar Clientes...";
+            this.buscarClientesToolStripMenuItem.Click += new System.EventHandler(this.buscarClientesToolStripMenuItem_Click);
+            // 
+            // listadoDeClientesDeudoresToolStripMenuItem
+            // 
+            this.listadoDeClientesDeudoresToolStripMenuItem.Name = "listadoDeClientesDeudoresToolStripMenuItem";
+            this.listadoDeClientesDeudoresToolStripMenuItem.Size = new System.Drawing.Size(280, 24);
+            this.listadoDeClientesDeudoresToolStripMenuItem.Text = "Listado de Cantidad de Clientes...";
+            this.listadoDeClientesDeudoresToolStripMenuItem.Click += new System.EventHandler(this.listadoDeClientesDeudoresToolStripMenuItem_Click);
+            // 
+            // listadoDeClientesDeUnaCiudadToolStripMenuItem
+            // 
+            this.listadoDeClientesDeUnaCiudadToolStripMenuItem.Name = "listadoDeClientesDeUnaCiudadToolStripMenuItem";
+            this.listadoDeClientesDeUnaCiudadToolStripMenuItem.Size = new System.Drawing.Size(280, 24);
+            this.listadoDeClientesDeUnaCiudadToolStripMenuItem.Text = "Listado de clientes de un Barrio...";
+            this.listadoDeClientesDeUnaCiudadToolStripMenuItem.Click += new System.EventHandler(this.listadoDeClientesDeUnaCiudadToolStripMenuItem_Click);
+            // 
+            // pbImagen
+            // 
+            this.pbImagen.Image = global::pryMamondezIEFI.Properties.Resources._33_336464_gym_high_quality_png_photo_logos_de_gimnasio;
+            this.pbImagen.Location = new System.Drawing.Point(0, 30);
+            this.pbImagen.Name = "pbImagen";
+            this.pbImagen.Size = new System.Drawing.Size(420, 411);
+            this.pbImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImagen.TabIndex = 1;
+            this.pbImagen.TabStop = false;
+            // 
+            // statusConexion
+            // 
+            this.statusConexion.Location = new System.Drawing.Point(0, 444);
+            this.statusConexion.Name = "statusConexion";
+            this.statusConexion.Size = new System.Drawing.Size(420, 22);
+            this.statusConexion.TabIndex = 2;
+            this.statusConexion.Text = "statusStrip1";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.BackColor = System.Drawing.Color.White;
+            this.lblEstado.Location = new System.Drawing.Point(12, 448);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(10, 13);
+            this.lblEstado.TabIndex = 3;
+            this.lblEstado.Text = "-";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(420, 466);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.statusConexion);
+            this.Controls.Add(this.pbImagen);
+            this.Controls.Add(this.msMain);
+            this.MainMenuStrip = this.msMain;
+            this.Name = "frmPrincipal";
+            this.Text = "Gimnasio";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.msMain.ResumeLayout(false);
+            this.msMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip msMain;
+        private System.Windows.Forms.ToolStripMenuItem sistemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarNuevosClientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buscarClientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listadoDeClientesDeudoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listadoDeClientesDeUnaCiudadToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pbImagen;
+        private System.Windows.Forms.StatusStrip statusConexion;
+        private System.Windows.Forms.Label lblEstado;
+    }
+}
+
