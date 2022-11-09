c937de3d5a479610e618a3794ac4f05590460f82
RomanMamondez <112417045+Romannn10@users.noreply.github.com>
2022-11-08 22:14:08 -0300

    Add files via upload

---
+++ pryMamondezIEFI/frmAgregarCliente.cs
@@ -0,0 +1,68 @@
+﻿using pryMamondezIEFI.Resources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryMamondezIEFI
+{
+    public partial class frmAgregarCliente : Form
+    {
+        public frmAgregarCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdCargar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Los datos se cargaron con exito");
+            txtDNI.Text = "";
+            txtNyANuevoCliente.Text = "";
+            txtDireccion.Text = "";
+            mskDNI.Text = "";
+            mskEdad.Text = "";
+            lstActividad.SelectedIndex = -1;
+            lstBarrio.SelectedIndex = -1;
+            this.Close();
+        }
+
+        private void frmAgregarCliente_Load(object sender, EventArgs e)
+        {
+            clsBarrio Barrio = new clsBarrio();
+            Barrio.Listar(lstBarrio);
+            clsActividad ActividadD = new clsActividad();
+            ActividadD.ListarActividad(lstActividad);
+            lstActividad.SelectedIndex = -1;
+            lstBarrio.SelectedIndex = -1;
+        }
+
+        private void lstActividad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstBarrio_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdAgregarNuevo_Click(object sender, EventArgs e)
+        {
+            clsAgregarCliente Registro = new clsAgregarCliente(); 
+            Registro.Dni = Convert.ToInt32(mskDNI.Text);
+            Registro.NyA = txtNyANuevoCliente.Text;
+            Registro.Direc = txtDireccion.Text;
+            Registro.Barrio = Convert.ToInt32(lstBarrio.SelectedValue);
+            Registro.Act = Convert.ToInt32(lstActividad.SelectedValue);
+            Registro.Monto = Convert.ToDecimal(txtPagara.Text);
+            Registro.EdadCliente = Convert.ToInt32(mskEdad.Text);
+            Registro.Agregar();
+
+        }
+    }
+}
---
+++ pryMamondezIEFI/frmClientesPorBarrio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using pryMamondezIEFI.Resources;
+
+namespace pryMamondezIEFI
+{
+    public partial class frmClientesPorBarrio : Form
+    {
+        public frmClientesPorBarrio()
+        {
+            InitializeComponent();
+        }
+
+        private void frmClientesPorBarrio_Load(object sender, EventArgs e)
+        {
+            clsBarrio Barrio = new clsBarrio();
+            Barrio.Listar(lstBarrio);
+            lstBarrio.SelectedIndex = -1;
+        }
+
+        private void lstBarrio_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdListar_Click(object sender, EventArgs e)
+        {
+            Int32 CodigoBarrio = Convert.ToInt32(lstBarrio.SelectedValue);
+            ClsClientes objListadoBarrios = new ClsClientes();
+            objListadoBarrios.ListarPorBarrio(dgvListadoBarrio, CodigoBarrio);
+        }
+    }
+}
