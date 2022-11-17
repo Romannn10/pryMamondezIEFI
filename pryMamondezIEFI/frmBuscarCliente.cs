d4dedfbc1d5addd53aa77fcbe63cb71bcb3e2baf
RomanMamondez <112417045+Romannn10@users.noreply.github.com>
2022-11-17 00:07:26 -0300

    Add files via upload

---
+++ pryMamondezIEFI/frmBuscarCliente.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using pryMamondezIEFI.Resources;
+
+namespace pryMamondezIEFI
+{
+    public partial class frmBuscarCliente : Form
+    {
+        public frmBuscarCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void mrcDatosDeCliente_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmBuscarCliente_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdGuardar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cmdBuscar_Click(object sender, EventArgs e)
+        {
+            //Llamo a la clase y al procedimiento BUSCAR
+            //traera la informacion que tengo cargadas en las
+            //variables de la clase y posteriormente las mostrara
+            Int32 DNI = Convert.ToInt32(mskDNICliente.Text);
+            clsAgregarCliente Buscar = new clsAgregarCliente();
+            Buscar.Buscar(DNI);
+
+            if (Buscar.Dni != DNI)
+            {
+                MessageBox.Show("El cliente no se encuentra en la base de datos");
+            }
+            else
+            {
+                MessageBox.Show("Datos encontrados con exito");
+                txtNombre.Text = Buscar.NyA;
+                clsBarrio Barrio = new clsBarrio();
+                txtBarrio.Text = Barrio.Buscar(Buscar.Barrio);
+                txtEdad.Text = Convert.ToString(Buscar.EdadCliente);
+                clsActividad Actividades = new clsActividad();
+                txtActividad.Text = Actividades.Buscar(Buscar.Act);
+                txtSaldo.Text = Convert.ToString(Buscar.Monto);
+            }
+        }
+
+        private void cmdSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cmdModificar_Click(object sender, EventArgs e)
+        {
+            //Llamo a la clase y al procedimiento
+            Int32 Cliente = Convert.ToInt32(mskDNICliente.Text);
+            clsAgregarCliente Modificar = new clsAgregarCliente();
+            //Le digo que llene los campos con los txt
+            Modificar.NyA = txtNombre.Text;
+            Modificar.Barrio = Convert.ToInt32(txtBarrio.Text);
+            Modificar.Act = Convert.ToInt32(txtActividad.Text);
+            Modificar.Monto = Convert.ToInt32(txtSaldo.Text);
+            Modificar.Modificar(Cliente);
+        }
+
+        private void cmdEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ pryMamondezIEFI/frmClientesPorBarrio.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace pryMamondezIEFI
+{
+    partial class frmClientesPorBarrio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBarrio = new System.Windows.Forms.Label();
+            this.lstBarrio = new System.Windows.Forms.ComboBox();
+            this.dgvListadoBarrio = new System.Windows.Forms.DataGridView();
+            this.DNI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NyA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Barrio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Actividad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Saldo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Edad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cmdGenerarReporte = new System.Windows.Forms.Button();
+            this.cmdListar = new System.Windows.Forms.Button();
+            this.cmdSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoBarrio)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblBarrio
+            // 
+            this.lblBarrio.AutoSize = true;
+            this.lblBarrio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBarrio.ForeColor = System.Drawing.Color.White;
+            this.lblBarrio.Location = new System.Drawing.Point(26, 29);
+            this.lblBarrio.Name = "lblBarrio";
+            this.lblBarrio.Size = new System.Drawing.Size(50, 17);
+            this.lblBarrio.TabIndex = 0;
+            this.lblBarrio.Text = "Barrio:";
+            // 
+            // lstBarrio
+            // 
+            this.lstBarrio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.lstBarrio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBarrio.FormattingEnabled = true;
+            this.lstBarrio.Location = new System.Drawing.Point(88, 26);
+            this.lstBarrio.Name = "lstBarrio";
+            this.lstBarrio.Size = new System.Drawing.Size(669, 24);
+            this.lstBarrio.TabIndex = 1;
+            this.lstBarrio.SelectedIndexChanged += new System.EventHandler(this.lstBarrio_SelectedIndexChanged);
+            // 
+            // dgvListadoBarrio
+            // 
+            this.dgvListadoBarrio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListadoBarrio.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DNI,
+            this.NyA,
+            this.Direccion,
+            this.Barrio,
+            this.Actividad,
+            this.Saldo,
+            this.Edad});
+            this.dgvListadoBarrio.Location = new System.Drawing.Point(29, 67);
+            this.dgvListadoBarrio.Name = "dgvListadoBarrio";
+            this.dgvListadoBarrio.Size = new System.Drawing.Size(728, 341);
+            this.dgvListadoBarrio.TabIndex = 2;
+            // 
+            // DNI
+            // 
+            this.DNI.HeaderText = "DNI";
+            this.DNI.Name = "DNI";
+            // 
+            // NyA
+            // 
+            this.NyA.HeaderText = "Nombre Y Apellido";
+            this.NyA.Name = "NyA";
+            // 
+            // Direccion
+            // 
+            this.Direccion.HeaderText = "Direccion";
+            this.Direccion.Name = "Direccion";
+            // 
+            // Barrio
+            // 
+            this.Barrio.HeaderText = "Barrio";
+            this.Barrio.Name = "Barrio";
+            // 
+            // Actividad
+            // 
+            this.Actividad.HeaderText = "Actividad";
+            this.Actividad.Name = "Actividad";
+            // 
+            // Saldo
+            // 
+            this.Saldo.HeaderText = "Saldo";
+            this.Saldo.Name = "Saldo";
+            // 
+            // Edad
+            // 
+            this.Edad.HeaderText = "Edad";
+            this.Edad.Name = "Edad";
+            // 
+            // cmdGenerarReporte
+            // 
+            this.cmdGenerarReporte.Location = new System.Drawing.Point(529, 415);
+            this.cmdGenerarReporte.Name = "cmdGenerarReporte";
+            this.cmdGenerarReporte.Size = new System.Drawing.Size(105, 32);
+            this.cmdGenerarReporte.TabIndex = 3;
+            this.cmdGenerarReporte.Text = "Generar Reporte";
+            this.cmdGenerarReporte.UseVisualStyleBackColor = true;
+            // 
+            // cmdListar
+            // 
+            this.cmdListar.Location = new System.Drawing.Point(652, 415);
+            this.cmdListar.Name = "cmdListar";
+            this.cmdListar.Size = new System.Drawing.Size(105, 32);
+            this.cmdListar.TabIndex = 4;
+            this.cmdListar.Text = "Listar";
+            this.cmdListar.UseVisualStyleBackColor = true;
+            this.cmdListar.Click += new System.EventHandler(this.cmdListar_Click);
+            // 
+            // cmdSalir
+            // 
+            this.cmdSalir.Location = new System.Drawing.Point(403, 415);
+            this.cmdSalir.Name = "cmdSalir";
+            this.cmdSalir.Size = new System.Drawing.Size(105, 32);
+            this.cmdSalir.TabIndex = 5;
+            this.cmdSalir.Text = "Salir";
+            this.cmdSalir.UseVisualStyleBackColor = true;
+            this.cmdSalir.Click += new System.EventHandler(this.cmdSalir_Click);
+            // 
+            // frmClientesPorBarrio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(800, 462);
+            this.Controls.Add(this.cmdSalir);
+            this.Controls.Add(this.cmdListar);
+            this.Controls.Add(this.cmdGenerarReporte);
+            this.Controls.Add(this.dgvListadoBarrio);
+            this.Controls.Add(this.lstBarrio);
+            this.Controls.Add(this.lblBarrio);
+            this.Name = "frmClientesPorBarrio";
+            this.Text = "Listado de clientes de una ciudad";
+            this.Load += new System.EventHandler(this.frmClientesPorBarrio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoBarrio)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBarrio;
+        private System.Windows.Forms.ComboBox lstBarrio;
+        private System.Windows.Forms.DataGridView dgvListadoBarrio;
+        private System.Windows.Forms.Button cmdGenerarReporte;
+        private System.Windows.Forms.Button cmdListar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DNI;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NyA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Direccion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Barrio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Actividad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Saldo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Edad;
+        private System.Windows.Forms.Button cmdSalir;
+    }
+}
